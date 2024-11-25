e0d7b33c355bed5efa28b3aab04c17cba1ff68ec
Danitinhg <dani111hg@gmail.com>
2024-11-25 18:07:58 +0100

    Inicio del programa 2

--- src/Main.kt
+++ src/Main.kt
@@ -1,20 +1,9 @@
 fun main() {
-    for (i in 1..100) {
 
-        var num: String
-        num = i.toString()
+    println("Introduzca una palabra")
+    val palabra1 = readln()
+    println("Introduzca una palabra")
+    val palabra2 = readln()
 
-        if (i % 3 == 0) {
-            num = "tri"
-        }
 
-        if (i % 5 == 0) {
-            num = "qui"
-        }
-
-        if (i % 3 == 0 && i % 5 == 0) {
-            num = "triqui"
-        }
-        println("$num")
-    }
 }
