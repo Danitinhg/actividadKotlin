a69ccf38a7a89ac3ed59b1e649b110171067285b
Danitinhg <dani111hg@gmail.com>
2024-11-25 19:21:26 +0100

    Creación del programa 3 y creación de lista de String con los caracteres de los mensajes

--- src/Main.kt
+++ src/Main.kt
@@ -1,46 +1,15 @@
 fun main() {
-    var anagrama: Boolean = true
-    var vocales: Int = 0
 
-    println("Introduzca una palabra: ")
-    val palabra1 = readln()
-    println("Introduzca otra palabra: ")
-    val palabra2 = readln()
+    
+}
 
-    val listPalabra1: MutableList<String> = mutableListOf()
-    val listPalabra2: MutableList<String> = mutableListOf()
+fun programa3 (mensaje: String) {
+    var cierres: Int = 0
 
-    for (i in palabra1.toCharArray().indices) {
-        val letra: Char = palabra1[i]
-        listPalabra1.add(letra.toString())
+    val listMensaje: MutableList<String> = mutableListOf()
+
+    for (i in mensaje.toCharArray().indices) {
+        val letra: Char = mensaje[i]
+        listMensaje.add(letra.toString())
     }
-
-    for (i in palabra2.toCharArray().indices) {
-        val letra: Char = palabra2[i]
-        listPalabra2.add(letra.toString())
-    }
-
-    for (i in listPalabra1) {
-       if (i == "a" || i == "e" || i == "i" || i == "o" || i == "u") {
-           vocales++
-       }
-    }
-
-    for (i in listPalabra2) {
-        if (i == "a" || i == "e" || i == "i" || i == "o" || i == "u") {
-            vocales++
-        }
-    }
-
-    if (vocales % 2 == 0) {
-        anagrama = true
-    } else {
-        anagrama = false
-    }
-
-    if (palabra1 == palabra2) {
-        anagrama = false
-    }
-
-    println("¿Las palabras $palabra1 y $palabra2 son un anagrama? = $anagrama")
 }
