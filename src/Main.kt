72a6c6ea5fb2b60ce2ee4e173d7c79c338dd57cb
Danitinhg <dani111hg@gmail.com>
2024-11-25 19:52:19 +0100

    Recorrido de mensajes y conteo de cierres, falta comprobar bien o no

--- src/Main.kt
+++ src/Main.kt
@@ -1,15 +1,50 @@
 fun main() {
-
-    
+    programa3("{a + b [c] * (2x2)}}}}")
+    programa3("{ [ a * ( c + d ) ] - 5 }")
+    programa3("{ a * ( c + d ) ] - 5 }")
+    programa3("{a^4 + (((ax4)}")
+    programa3("{ ] a * ( c + d ) + ( 2 - 3 )[ - 5 }")
+    programa3("{{{{{{(}}}}}}")
+    programa3("(a")
 }
 
 fun programa3 (mensaje: String) {
-    var cierres: Int = 0
+    var cierres1: Int = 0
+    var cierres2: Int = 0
+    var cierres3: Int = 0
+    var abierto: Boolean = false
 
     val listMensaje: MutableList<String> = mutableListOf()
 
     for (i in mensaje.toCharArray().indices) {
         val letra: Char = mensaje[i]
         listMensaje.add(letra.toString())
+    }
+
+    for (i in listMensaje) {
+        if (i == "(" || i == ")") {
+            cierres1++
+        }
+    }
+
+    for (i in listMensaje) {
+        if (i == "{" || i == "}") {
+            cierres2++
+        }
+    }
+
+    for (i in listMensaje) {
+        if (i == "[" || i == "]") {
+            cierres3++
+        }
+    }
+
+
+    if (cierres1 % 2 == 0 && cierres2 % 2 == 0 && cierres3 % 2 == 0) {
+        var balanceada: Boolean = true
+        println("$mensaje ¿esta balanceada? $balanceada")
+    } else {
+        var balanceada: Boolean = false
+        println("$mensaje ¿esta balanceada? $balanceada")
     }
 }
