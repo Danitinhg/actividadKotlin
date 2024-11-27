8b2be1133dd8be0bf5ed026cc800134fb30a9ce5
Danitinhg <dani111hg@gmail.com>
2024-11-27 17:49:52 +0100

    Eliminación de código erroneo

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,5 @@
 fun main() {
     var anagrama: Boolean = true
-    var vocales: Int = 0
 
     println("Introduzca una palabra: ")
     val palabra1 = readln()
@@ -19,25 +18,7 @@ fun main() {
         val letra: Char = palabra2[i]
         listPalabra2.add(letra.toString())
     }
-
-    for (i in listPalabra1) {
-       if (i == "a" || i == "e" || i == "i" || i == "o" || i == "u") {
-           vocales++
-       }
-    }
-
-    for (i in listPalabra2) {
-        if (i == "a" || i == "e" || i == "i" || i == "o" || i == "u") {
-            vocales++
-        }
-    }
-
-    if (vocales % 2 == 0) {
-        anagrama = true
-    } else {
-        anagrama = false
-    }
-
+    
     if (palabra1 == palabra2) {
         anagrama = false
     }
