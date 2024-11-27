6f37338a9ed9de03aeef4ea7ae5c0380fab22499
Danitinhg <dani111hg@gmail.com>
2024-11-27 18:00:17 +0100

    Añadido de for para recorrer la lista y contador para comprobar si contienen las mismas letras, programa 2 finalizado

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,6 @@
 fun main() {
-    var anagrama: Boolean = true
+    var anagrama: Boolean = false
+    var contador: Int = 0
 
     println("Introduzca una palabra: ")
     val palabra1 = readln()
@@ -18,7 +19,17 @@ fun main() {
         val letra: Char = palabra2[i]
         listPalabra2.add(letra.toString())
     }
-    
+
+    for (i in listPalabra1) {
+        if (listPalabra2.contains(i)) {
+            contador++
+        }
+    }
+
+    if (contador == listPalabra1.size) {
+        anagrama = true
+    }
+
     if (palabra1 == palabra2) {
         anagrama = false
     }
